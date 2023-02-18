9b0de6ea06449198c7a5c29baf0afdfbcc62367d
Black_Panda <piotrozawada97@gmail.com>
2023-02-18 12:25:10 +0100

    Adding and customing buttons are done completly.

--- main.py
+++ main.py
@@ -14,28 +14,45 @@ class RightFrame(csTK.CTkFrame):
 class LeftFrame(csTK.CTkFrame):
     def __init__(self, container):
         super().__init__(container)
-        self.invoke_widgets()
+# Adding Button Functions
+        def b1_func():
+            print("welcum")
 
-    def invoke_widgets(self):
+        def b2_func():
+            print("welcum")
+
+        def b3_func():
+            print("welcum")
+
 # Adding Button Images
         b1_img = tkinter.PhotoImage(file="img/delivery.png")
         b2_img = tkinter.PhotoImage(file="img/menu.png")
         b3_img = tkinter.PhotoImage(file="img/maintain.png")
-# Specyfing Button Font
+# Specyfing Button Font, Border Width, Colour
         button_font = csTK.CTkFont(family="Arial", size=40, weight="bold")
+        button_bd_width=3
+        button_bd_colour="#327F96"
+        button_bd_spacing=50
+        button_pad=3
 # Adding Buttons Themselves
         self.b1 = csTK.CTkButton(self,
                                  text="Order", font=button_font,
-                                 image=b1_img.subsample(6,6),
-                                 height=200).grid(row=0, column=0, columnspan=2, sticky="we")
+                                 image=b1_img.subsample(4,4),  fg_color="#7B3296",
+                                 height=200,
+                                 border_width=button_bd_width, border_spacing=button_bd_spacing, border_color=button_bd_colour,
+                                 command=b1_func).grid(row=0, column=0, columnspan=2, sticky="we", padx=(button_pad), pady=(button_pad))
         self.b2 = csTK.CTkButton(self,
                                  text="Menu", font=button_font,
-                                 image=b2_img.subsample(6,6),
-                                 width=200, height=200).grid(row=1, column=0)
+                                 image=b2_img.subsample(6,6),  fg_color="#964932",
+                                 width=200, height=200,
+                                 border_width=button_bd_width, border_spacing=button_bd_spacing, border_color=button_bd_colour,
+                                 command=b2_func).grid(row=1, column=0, padx=(button_pad), pady=(button_pad))
         self.b3 = csTK.CTkButton(self,
                                  text="Pizza Maintain", font=button_font,
-                                 image=b3_img.subsample(6,6),
-                                 width=200, height=200).grid(row=1, column=1)
+                                 image=b3_img.subsample(6,6),  fg_color="#4D9632",
+                                 width=200, height=200,
+                                 border_width=button_bd_width, border_spacing=button_bd_spacing, border_color=button_bd_colour,
+                                 command=b3_func).grid(row=1, column=1, padx=(button_pad), pady=(button_pad))
 
 
 class MainWindow(csTK.CTk):
@@ -43,7 +60,7 @@ class MainWindow(csTK.CTk):
         super().__init__()
 # Main Window Configuration
         self.title("RODO Agreement Auto-Filler")
-        self.minsize(1000, 400)
+        self.minsize(1700, 400)
 
         self.invoke_widgets()
 
