292ed01f0427c2c4f09ecbbfd915c00827039c1d
Black_Panda <piotrozawada97@gmail.com>
2023-02-18 11:57:29 +0100

    First sucessfull button adding with buttons and proper layout. Changed tkinter into customtkinter

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import tkinter
-from tkinter import *
-from tkinter import ttk
+import customtkinter as csTK
 
 
-class RightFrame(ttk.Frame):
+class RightFrame(csTK.CTkFrame):
     def __init__(self, container):
         super().__init__(container)
         self.invoke_widget()
 
     def invoke_widget(self):
-        self.label = Label(self, text="Welcome").grid(row=0, column=0)
+        self.label = csTK.CTkLabel(self, text="Welcome").grid(row=0, column=0)
 
 
-class LeftFrame(tkinter.Tk):
+class LeftFrame(csTK.CTkFrame):
     def __init__(self, container):
         super().__init__(container)
         self.invoke_widgets()
 
     def invoke_widgets(self):
-        b1_img = Image("img/delivery.png")
-        b1_img = b1_img.resize((100, 100))
-        self.b1 = Button(self, text="Order", image=b1_img, width=100, height=100).grid(row=0, column=0)
+# Adding Button Images
+        b1_img = tkinter.PhotoImage(file="img/delivery.png")
+        b2_img = tkinter.PhotoImage(file="img/menu.png")
+        b3_img = tkinter.PhotoImage(file="img/maintain.png")
+# Specyfing Button Font
+        button_font = csTK.CTkFont(family="Arial", size=40, weight="bold")
+# Adding Buttons Themselves
+        self.b1 = csTK.CTkButton(self,
+                                 text="Order", font=button_font,
+                                 image=b1_img.subsample(6,6),
+                                 height=200).grid(row=0, column=0, columnspan=2, sticky="we")
+        self.b2 = csTK.CTkButton(self,
+                                 text="Menu", font=button_font,
+                                 image=b2_img.subsample(6,6),
+                                 width=200, height=200).grid(row=1, column=0)
+        self.b3 = csTK.CTkButton(self,
+                                 text="Pizza Maintain", font=button_font,
+                                 image=b3_img.subsample(6,6),
+                                 width=200, height=200).grid(row=1, column=1)
 
-        self.b2 = Button(self, text="Menu").grid(row=0, column=1)
-        self.b3 = Button(self, text="Pizza Maintain").grid(row=1, column=0)
-        self.b4 = Button(self, text="Help").grid(row=1, column=1)
 
-
-class MainWindow(tkinter.Tk):
+class MainWindow(csTK.CTk):
     def __init__(self):
         super().__init__()
 # Main Window Configuration
