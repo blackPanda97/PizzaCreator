434c3531536335c914a0de44b6d796ff3af3953d
Black_Panda <piotrozawada97@gmail.com>
2023-03-12 14:22:10 +0100

    Unified colour scheme in maintain_frame.py

--- maintain_frame.py
+++ maintain_frame.py
@@ -7,45 +7,61 @@ class MaintainFrame(csTK.CTkFrame):
         super().__init__(container)
         self.configure(self, fg_color="#E6D386")
 
-        main_f = csTK.CTkFont(family="Arial", size=30, weight="bold")
+#Overall properties of buttons and labels
+        main_f = csTK.CTkFont(family="Arial", size=20, weight="bold")
+        selected_cl = "#671313"
+        unselected_cl = "#D1595A"
+        foreground_cl = "#E6D386"
+        unselected_hover_cl="#5C7758"
 
-        def b_choice(value):
-            print(value)
-            if value == "Add":
-                e = csTK.CTkEntry(self, )
+#Add or Remove Button Function
+        def b_choice(value, value2=0):
+            print(value, value2)
 
+#Segmented Button Function
         def op_choice(value):
             print(value)
 
 
         csTK.CTkLabel(self,
-                      text="Chose an item to maintain",
+                      text="Item to maintain",
                       text_color="black",
-                      font=main_f).grid(row=0, column=0, padx=5)
+                      font=main_f).grid(row=0, column=2, padx=5)
 
-        csTK.CTkOptionMenu(master=self,
+        csTK.CTkOptionMenu(self,
                                 values=("Meat Toppings", "Veggie Toppings", "Cheese Toppings", "Sauces"),
-                                font=main_f, fg_color="#7b3296",
-                                dropdown_fg_color="black",
-                                dropdown_font=csTK.CTkFont(family="Arial", size=15, weight="bold"),
-                                command=op_choice).grid(row=0, column=1, padx=5)
+                                font=main_f,
+                                fg_color=unselected_cl,
+                                button_color=unselected_cl,
+                                text_color="black",
+                                dropdown_fg_color=foreground_cl,
+                                dropdown_font=main_f,
+                                dropdown_text_color="black",
+                                command=op_choice).grid(row=1, column=2, padx=5)
 
+#Entry places and Labels
         csTK.CTkLabel(self,
                       text="Id",
-                      font=main_f,
-                      fg_color="black").grid(row=1, column=0)
+                      text_color="black",
+                      font=main_f).grid(row=0, column=0)
         csTK.CTkLabel(self,
                       text="Name",
-                      font=main_f,
-                      fg_color="black").grid(row=2, column=0)
+                      text_color="black",
+                      font=main_f).grid(row=1, column=0)
 
         csTK.CTkEntry(self,
-                      placeholder_text="Id").grid(row=1, column=1)
+                      placeholder_text="Id",
+                      font=main_f).grid(row=0, column=1)
         csTK.CTkEntry(self,
-                      placeholder_text="Name").grid(row=2, column=1)
+                      placeholder_text="Name",
+                      font=main_f).grid(row=1, column=1)
 
+#Buttons to submit the action
         csTK.CTkSegmentedButton(self,
                                 values=["Add", "Remove"],
                                 font=main_f,
-                                fg_color="#7b3296",
-                                command=b_choice).grid(row=3, column=0, padx=5, columnspan=2)
+                                fg_color=foreground_cl,
+                                selected_color=selected_cl,
+                                unselected_color=unselected_cl,
+                                unselected_hover_color=unselected_hover_cl,
+                                command=b_choice).grid(row=3, column=0, padx=5, columnspan=4)
